b49235271bf3603d5f6f15ae9d3fb5e46b79c06b
Pepe <josepp2508@gmail.com>
2026-03-06 17:38:17 -0600

    [IMP] changed error code 4 details

--- src/api/types/errors.ts
+++ src/api/types/errors.ts
@@ -1,7 +1,7 @@
 export enum WialonErrorCode {
   SUCCESSFUL_OPERATION = 0,
   UNKNOWN_ERROR = 6,
-  WRONG_TOKEN_LENGTH = 4,
+  INVALID_INPUT = 4,
   REQUEST_LIMIT = 1003,
   ACCESS_DENIED = 7,
   SUBUSER_NOT_FOUND = 8,
@@ -9,8 +9,8 @@ export enum WialonErrorCode {
 
 export const WialonErrorMessages: Record<number, string> = {
   [WialonErrorCode.SUCCESSFUL_OPERATION]: 'Successful operation',
+  [WialonErrorCode.INVALID_INPUT]: 'Invalid session | item not found | unknown error | internal error | non authorized | wrong token lenght',
   [WialonErrorCode.UNKNOWN_ERROR]: 'Unknown error ocurred',
-  [WialonErrorCode.WRONG_TOKEN_LENGTH]: 'Invalid token length',
   [WialonErrorCode.REQUEST_LIMIT]: 'Request limit exceeded',
   [WialonErrorCode.ACCESS_DENIED]: 'Access denied - token not activated or no service access',
   [WialonErrorCode.SUBUSER_NOT_FOUND]: 'Subuser not found or insufficient access rights',
