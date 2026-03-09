740f61b926062654d93bd45bca07c02380344276
Pepe <j.reyes@solucionescomsys.com>
2026-03-09 12:11:43 -0600

    [ADD] sdk imports

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+// Main API exports
+export { CoreAPI, WialonAuthError } from './src/api/core/core';
+export { AuthAPI } from './src/api/core/services/auth';
+
+// Type exports
+export type {
+  ILoginParams,
+  ILoginResponse,
+  ILoginResult
+} from './src/api/interfaces/core.interface';
+
+export { WialonErrorCode, WialonErrorMessages } from './src/api/types/errors';
