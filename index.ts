3939a407361222b5bf53dbc928848356c00a2aad
pepe <josepp2508@gmail.com>
2026-03-12 12:32:48 -0600

    [IMP] new api exports

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 // Main API exports
-export { CoreAPI, WialonAuthError } from './src/api/core/core';
-export { AuthAPI } from './src/api/core/services/auth';
+export { WialonAPI, WialonAuthError } from "./src/api/core/core";
+export { AuthApi } from "./src/api/core/services/auth";
+export { ReportsApi } from "./src/api/core/services/reports";
+export { UnitApi } from "./src/api/core/services/units";
 
 // Type exports
 export type {
   ILoginParams,
   ILoginResponse,
-  ILoginResult
-} from './src/api/interfaces/core.interface';
+  ILoginResult,
+} from "./src/api/interfaces/core.interface";
 
-export { WialonErrorCode, WialonErrorMessages } from './src/api/types/errors';
+export type { IGroupData } from "./src/api/interfaces/units.interface";
+
+export { WialonErrorCode, WialonErrorMessages } from "./src/api/types/errors";
