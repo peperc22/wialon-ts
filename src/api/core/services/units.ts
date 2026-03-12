8b68a520c351835c614fdd665232dccf30fdfa2a
pepe <josepp2508@gmail.com>
2026-03-12 11:17:19 -0600

    [LINT] units.ts linting phase

--- src/api/core/services/units.ts
+++ src/api/core/services/units.ts
@@ -1,9 +1,10 @@
 import type { AxiosInstance } from "axios";
 import { WialonAuthError } from "../core";
 import { WialonErrorCode } from "../../types/errors";
+import type { IGroupData } from "../../interfaces/units.interface";
 
 export class UnitApi {
-  constructor(private client: AxiosInstance) { }
+  constructor(private client: AxiosInstance) {}
 
   async GetIdByName(sid: string, unitName: string): Promise<number> {
     const params = {
@@ -11,39 +12,37 @@ export class UnitApi {
         itemsType: "avl_unit",
         propName: "sys_name",
         propValueMask: unitName,
-        sortType: ""
+        sortType: "",
       },
       force: 1,
       flags: 1,
       from: 0,
-      to: 0
+      to: 0,
     };
 
     try {
-      const response = await this.client.get('', {
+      const response = await this.client.get("", {
         params: {
-          svc: 'core/search_items',
+          svc: "core/search_items",
           params: JSON.stringify(params),
-          sid: sid
-        }
+          sid: sid,
+        },
       });
 
-      if ('error' in response.data)
+      if ("error" in response.data)
         throw new WialonAuthError(response.data.error);
 
       const id = response.data.items[0].id;
 
       return id;
     } catch (error) {
-      if (error instanceof WialonAuthError)
-        throw error;
+      if (error instanceof WialonAuthError) throw error;
 
       throw new WialonAuthError(
         WialonErrorCode.UNKNOWN_ERROR,
-        'Unexpected error during unit search',
-        error
+        "Unexpected error during unit search",
+        error,
       );
     }
   }
 }
-
