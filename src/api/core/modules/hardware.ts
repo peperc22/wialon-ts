de890997be469e6a25c618d8aada7c739e7da4a7
pepe <josepp2508@gmail.com>
2026-05-14 16:33:45 -0600

    [FEAT] retrieves the gpd model based on the given hardware id
    Signed-off-by: pepe <josepp2508@gmail.com>

---
+++ src/api/core/modules/hardware.ts
@@ -0,0 +1,44 @@
+import type { AxiosInstance } from "axios";
+import { WialonAuthError } from "../core";
+import { WialonErrorCode } from "../../types/errors";
+
+export class HardwareApi {
+  constructor(private client: AxiosInstance) {}
+
+  async getHardwareInfo(
+    sid: string,
+    hardwareId: number,
+  ): Promise<{ model: string }> {
+    const params = {
+      filterType: "id",
+      filterValue: [hardwareId],
+      includeType: true,
+    };
+
+    try {
+      const response = await this.client.get("", {
+        params: {
+          svc: "core/get_hw_types",
+          params: JSON.stringify(params),
+          sid: sid,
+        },
+      });
+
+      if ("error" in response.data) {
+        throw new Error(`Error fetching hardware info: ${response.data.error}`);
+      }
+
+      const hardwareInfo = response.data[0]?.name;
+
+      return { model: hardwareInfo };
+    } catch (error) {
+      if (error instanceof WialonAuthError) throw error;
+
+      throw new WialonAuthError(
+        WialonErrorCode.UNKNOWN_ERROR,
+        "Unexpected error during hardware info retrieval",
+        error,
+      );
+    }
+  }
+}
