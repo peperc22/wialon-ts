d573a2b0d38c9c852d739948a52e7b1c3a1d0099
pepe <josepp2508@gmail.com>
2026-05-15 12:53:38 -0600

    [ADD] unit data and commands data interface
    Signed-off-by: pepe <josepp2508@gmail.com>

--- src/api/interfaces/units.interface.ts
+++ src/api/interfaces/units.interface.ts
@@ -1,5 +1,13 @@
+import type { ICommand } from "./commands.interface";
+
 export interface IGroupData {
   groupName: string;
   groupId: string;
   unitIds: number[];
+}
+
+export interface IUnitData {
+  id: number;
+  gpsId: number;
+  commands: ICommand[];
 }
