86b51a19c4c9e55fc626d012706bb07691f21898
pepe <josepp2508@gmail.com>
2026-06-23 15:59:43 -0600

    [IMP] reformat index.ts and adopt @/ alias for UnitApi
    Signed-off-by: pepe <josepp2508@gmail.com>

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 // Main API exports
-export { WialonApi, WialonError } from "./src/api/core/core";
-export { AuthApi } from "./src/api/core/modules/auth";
-export { ReportsApi } from "./src/api/core/modules/reports";
-export { UnitApi } from "./src/api/core/modules/units";
+export {WialonApi, WialonError} from "./src/api/core/core";
+export {AuthApi} from "./src/api/core/modules/auth";
+export {ReportsApi} from "./src/api/core/modules/reports";
+export {UnitApi} from "@/api/core/modules/units";
 
 // Type exports
 export type {
-  ILoginParams,
-  ILoginResponse,
-  ILoginResult,
+    ILoginParams,
+    ILoginResponse,
+    ILoginResult,
 } from "./src/api/interfaces/core.interface";
 
-export type { IGroupData } from "./src/api/interfaces/units.interface";
+export type {IGroupData} from "./src/api/interfaces/units.interface";
 
-export { WialonErrorCode, WialonErrorMessages } from "./src/api/types/errors";
+export {WialonErrorCode, WialonErrorMessages} from "./src/api/types/errors";
