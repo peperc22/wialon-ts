fa2bb5a6bfcb28265fa9ba8a2071f60e752abab3
pepe <josepp2508@gmail.com>
2026-06-24 16:31:01 -0600

    [REF] find single unit now works for name & custom field
    Signed-off-by: pepe <josepp2508@gmail.com>

--- src/api/core/modules/units/index.ts
+++ src/api/core/modules/units/index.ts
@@ -1,20 +1,35 @@
 import type {HttpClient} from "@/config/http-client.ts";
 import {getUnitGroup} from "@/api/core/modules/units/get-unit-group.ts";
 import {executeCommand} from "@/api/core/modules/units/execute-command.ts";
-import type {IGroupData, IUnitData} from "@/api/interfaces/units.interface.ts";
-import {findUnitByName} from "@/api/core/modules/units/find-unit-by-name.ts";
+import type {IGroupData, IUnitData, UnitSearchStrategy} from "@/api/interfaces/units.interface.ts";
 import type {CommandProtocolType} from "@/api/types/commands.ts";
+import {BY_CUSTOM_FIELD, BY_NAME, findUnit} from "@/api/core/modules/units/find-unit.ts";
 
 export class UnitApi {
     constructor(private client: HttpClient) {
     }
+
+    findUnit = (
+        sid: string,
+        value: string,
+        strategy: UnitSearchStrategy,
+        withDetails?: boolean,
+    ): Promise<number | IUnitData> =>
+        findUnit(this.client, sid, value, strategy, withDetails);
 
     findUnitByName = (
         sid: string,
         unitName: string,
         withDetails?: boolean,
     ): Promise<number | IUnitData> =>
-        findUnitByName(this.client, sid, unitName, withDetails);
+        findUnit(this.client, sid, unitName, BY_NAME, withDetails);
+
+    findUnitByCustomField = (
+        sid: string,
+        value: string,
+        withDetails?: boolean,
+    ): Promise<number | IUnitData> =>
+        findUnit(this.client, sid, value, BY_CUSTOM_FIELD, withDetails);
 
     getUnitGroup = (
         sid: string,
