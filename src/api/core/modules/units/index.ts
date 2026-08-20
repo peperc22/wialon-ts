57f7d1b75fee80a47454a5e19aba704eb2af4a0f
pepe <josepp2508@gmail.com>
2026-08-20 15:10:38 -0600

    [IMP] execute command failure now returns the code instead of bool
    Signed-off-by: pepe <josepp2508@gmail.com>

--- src/api/core/modules/units/index.ts
+++ src/api/core/modules/units/index.ts
@@ -1,9 +1,9 @@
-import type {HttpClient} from "../../../../config/http-client.ts";
-import {getUnitGroup} from "./get-unit-group.ts";
-import {executeCommand} from "./execute-command.ts";
-import type {IGroupData, IUnitData, UnitSearchStrategy} from "../../../interfaces/units.interface.ts";
-import type {CommandProtocolType} from "../../../types/commands.ts";
-import {BY_CUSTOM_FIELD, BY_NAME, findUnit} from "./find-unit.ts";
+import type { HttpClient } from "../../../../config/http-client.ts";
+import { getUnitGroup } from "./get-unit-group.ts";
+import { executeCommand } from "./execute-command.ts";
+import type { IGroupData, IUnitData, UnitSearchStrategy } from "../../../interfaces/units.interface.ts";
+import type { CommandProtocolType } from "../../../types/commands.ts";
+import { BY_CUSTOM_FIELD, BY_NAME, findUnit } from "./find-unit.ts";
 
 export class UnitApi {
     constructor(private client: HttpClient) {
@@ -45,6 +45,6 @@ export class UnitApi {
         protocolType: CommandProtocolType,
         timeout: number,
         flags?: number,
-    ): Promise<boolean> =>
+    ): Promise<boolean | unknown> =>
         executeCommand(this.client, sid, commandName, commandParameter, unitId, protocolType, timeout, flags)
 }
