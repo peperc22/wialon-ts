f678fd043c227e3cd5995869fcf9b081163fc2cf
MauricioV <m.ventura@solucionescomsys.com>
2026-08-20 15:49:11 -0600

    feat(units): add sensor response types
    Signed-off-by: MauricioV <m.ventura@solucionescomsys.com>

--- src/api/interfaces/units.interface.ts
+++ src/api/interfaces/units.interface.ts
@@ -47,3 +47,39 @@ export interface UnitSearchItem {
     pos?: { t: number };
     netconn?: number;
 }
+
+export interface IUnitsSensors {
+    nm: string;
+    cls: number;
+    id: number;
+    mu: number;
+    sens: Record<string, IWialonSensorData>;
+}
+
+export interface IWialonSensorData {
+    id: number;
+    n: string;
+    t: string;
+    d: string;
+    m: string;
+    p: string;
+    f: number;
+    c: string;
+    vt: number;
+    vs: number;
+    tbl: unknown[];
+    ct: number;
+    mt: number;
+}
+
+export interface IUnitsLastSensorsValues {
+    i: number;
+    sensors: Record<string, IWialonSensorValues>;
+}
+
+export interface IWialonSensorValues {
+    value: string | number;
+    format: {
+        value: string;
+    };
+}
