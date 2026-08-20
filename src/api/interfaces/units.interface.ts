07388a3985f954fb1ea02557b314ea0b37160d34
MauricioV <m.ventura@solucionescomsys.com>
2026-08-20 12:02:05 -0600

    ADD: add new interfaces
    Signed-off-by: MauricioV <m.ventura@solucionescomsys.com>

--- src/api/interfaces/units.interface.ts
+++ src/api/interfaces/units.interface.ts
@@ -3,3 +3,39 @@ export interface IGroupData {
   groupId: string;
   unitIds: number[];
 }
+
+export interface IUnitsSensors {
+  nm: string;
+  cls: number;
+  id: number;
+  mu: number;
+  sens: Record<string, IWialonSensorData>;
+}
+
+export interface IWialonSensorData {
+  id: number;
+  n: string;
+  t: string;
+  d: string;
+  m: string;
+  p: string;
+  f: number;
+  c: string;
+  vt: number;
+  vs: number;
+  tbl: unknown[];
+  ct: number;
+  mt: number;
+}
+
+export interface IUnitsLastSensorsValues {
+  i: number;
+  sensors: Record<string, IWialonSensorValues>
+}
+
+export interface IWialonSensorValues {
+  value: string | number,
+  format: {
+    value: string
+  }
+}
