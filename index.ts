b83dee6b47c8291369b5fbf79c2a9e63af1cfa3d
MauricioV <m.ventura@solucionescomsys.com>
2026-08-20 12:00:38 -0600

    ADD: add new types to export from units.interface
    Signed-off-by: MauricioV <m.ventura@solucionescomsys.com>

--- index.ts
+++ index.ts
@@ -11,6 +11,6 @@ export type {
   ILoginResult,
 } from "./src/api/interfaces/core.interface";
 
-export type { IGroupData } from "./src/api/interfaces/units.interface";
+export type { IGroupData, IUnitsSensors, IUnitsLastSensorsValues } from "./src/api/interfaces/units.interface";
 
 export { WialonErrorCode, WialonErrorMessages } from "./src/api/types/errors";
